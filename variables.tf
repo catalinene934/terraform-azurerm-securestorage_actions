c2dd7bd9f877884ac49e02c36988755066c053a1
Catalin Ene <catalin_rasvan_ene@yahoo.com>
2026-01-15 09:50:05 +0200

    fix account_tier - Standard

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "storage_account_name" {
 }
 variable "account_tier" {
   type        = string
-  default     = "standard"
+  default     = "Standard"
   description = "Storage account name"
 
 }
