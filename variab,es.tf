ffb436394293c0e01c41f5cff7212e19f2b81dd6
Catalin Ene <catalin_rasvan_ene@yahoo.com>
2026-01-15 08:38:43 +0200

    version 1.0.0

---
+++ variab,es.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group"
+
+}
+
+variable "location" {
+  type        = string
+  description = "Location desc"
+
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage account name"
+
+}
+variable "account_tier" {
+  type        = string
+  default     = "standard"
+  description = "Storage account name"
+
+}
+
+variable "account_replication_type" {
+  type        = string
+  default     = "GRS"
+  description = "Replication Type"
+
+}
